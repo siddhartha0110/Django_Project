ae333943d7797451bb1d542d3714cc817070c050
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2020-02-26 15:22:08 +0530

    Displaying Data from Database and Pagination

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Display All the Listings
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 
 # Display one particular listing
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listing = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listing
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
